6a1ce1ba8c96e8e0dea43853c64845e47f11294a
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-17 19:53:22 +0300

    receipt element partially done

--- src/App.js
+++ src/App.js
@@ -10,12 +10,29 @@ function App() {
           <img src={logo} className="microsel-logo"></img>
         </div>
         <div className="receipt-element">
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className="receipt-title">Ödeme Özeti</div>
+          <div className="receipt-info">
+            <div className="receipt-line-info">
+              <div className="receipt-line-text">Sipariş Numarası</div>
+              <div className="receipt-line-output">11458523</div>
+            </div>
+            <div className="receipt-line-info">
+              <div className="receipt-line-text">KDV</div>
+              <div className="receipt-line-output">%20</div>
+            </div>
+            <div className="receipt-line-info">
+              <div className="receipt-line-text">KDV Tutari</div>
+              <div className="receipt-line-output">$ 123,28</div>
+            </div>
+            <div className="receipt-line-info">
+              <div className="receipt-line-text">Sipariş Tutarı</div>
+              <div className="receipt-line-output">$ 403,04</div>
+            </div>
+          </div>
+          {/* <div></div> */}
+          {/* <div></div> */}
+          {/* <div></div> */}
+          {/* <div></div> */}
         </div>
       </div>
     </div>
