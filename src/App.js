14b57344506f5e8a931af8890ec1f69efc300e84
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-17 18:50:32 +0300

    started creating receipt element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import './App.css';
+import logo from './image/microsel_logo.png';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="full-page">
+      <div className="main-element">
+        <div>
+          <img src={logo} className="microsel-logo"></img>
+        </div>
+        <div className="receipt-element">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default App;
