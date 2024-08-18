d0c3b066d44ae4107c5367acf719fe8532589fd6
DESKTOP-JGJSHGL\bih <peroxide614@gmail.com>
2024-08-18 21:04:10 +0300

    dashed line + pseudos + started third element

--- src/App.js
+++ src/App.js
@@ -9,30 +9,38 @@ function App() {
         <div>
           <img src={logo} className="microsel-logo"></img>
         </div>
-        <div className="receipt-element">
-          <div className="receipt-title">Ödeme Özeti</div>
-          <div className="receipt-info">
-            <div className="receipt-line-info">
-              <div className="receipt-line-text">Sipariş Numarası</div>
-              <div className="receipt-line-output">11458523</div>
-            </div>
-            <div className="receipt-line-info">
-              <div className="receipt-line-text">KDV</div>
-              <div className="receipt-line-output">%20</div>
-            </div>
-            <div className="receipt-line-info">
-              <div className="receipt-line-text">KDV Tutari</div>
-              <div className="receipt-line-output">$ 123,28</div>
-            </div>
-            <div className="receipt-line-info">
-              <div className="receipt-line-text">Sipariş Tutarı</div>
-              <div className="receipt-line-output">$ 403,04</div>
+        <div className="receipt-wrapper">
+          <div className="receipt-element">
+            <div className="first">
+              <div className="receipt-title">Ödeme Özeti</div>
+              <div className="receipt-info">
+                <div className="receipt-line-info">
+                  <div className="receipt-line-text">Sipariş Numarası</div>
+                  <div className="receipt-line-output">11458523</div>
+                </div>
+                <div className="receipt-line-info">
+                  <div className="receipt-line-text">KDV</div>
+                  <div className="receipt-line-output">%20</div>
+                </div>
+                <div className="receipt-line-info">
+                  <div className="receipt-line-text">KDV Tutari</div>
+                  <div className="receipt-line-output">$ 123,28</div>
+                </div>
+                <div className="receipt-line-info">
+                  <div className="receipt-line-text">Sipariş Tutarı</div>
+                  <div className="receipt-line-output">$ 403,04</div>
+                </div>
+              </div>
             </div>
           </div>
-          {/* <div></div> */}
-          {/* <div></div> */}
-          {/* <div></div> */}
-          {/* <div></div> */}
+          <div className="second-container">
+            <div className="dashed-line"></div>
+          </div>
+          <div className="third">
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </div>
